650ef9b806707f092722e52376d35aa5b2924fc7
FazalulAbid <fazalulabid007@gmail.com>
2022-10-11 11:46:00 +0530

    Initial commit (After SplashScreen and list card design).

---
+++ app/src/main/java/com/example/mytestappforwisdomleaf/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.mytestappforwisdomleaf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private static final long SPLASH_SCREEN_LENGTH = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Finish splash screen after 3 sec and goto MainActivity
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_SCREEN_LENGTH);
+    }
+}
